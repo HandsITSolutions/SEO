80905826100be12a5d4accb549cea9a430824fdc
Jamie Hands <Jamie@HITSolutionsLtd.com>
2014-02-21 10:58:52 +0000

    Initial Commit

---
+++ SEO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SEO
+//
+//  Created by Jamie Hands on 21/02/2014.
+//  Copyright (c) 2014 Chameleon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
